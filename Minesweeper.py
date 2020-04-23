77845828e98d4bd8eebabef2d1e0fb30bf03717e
Michael-Ivanov1 <michael.ivanov2002@gmail.com>
2020-04-23 14:59:46 -0700

    Second Commit - Clean up version

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,5 +1,4 @@
 import mines_array
-import check_mines
 import random
 import os
 
@@ -9,35 +8,35 @@ arr_mines = [[0 for i in range(10)] for j in range(10)]
 # array to be printed during the game
 arr_game = [["█" for a in range(10)] for b in range(10)]
 
-
 # place mines at random position
 for i in range(10):
     ran_X = random.randint(0, 9)
     ran_Y = random.randint(0, 9)
     arr_mines[ran_X][ran_Y] = 9
 
+count = 0
+for number in arr_mines:
+    if number == 9:
+        count += 1
+
+# Checks the position of mines and fills the numbers around them.
 for i in range(len(arr_mines)):
     for j in range(len(arr_mines[i])):
         if arr_mines[i][j] != 9:
-            check_mines.c_mines(i, j, arr_mines)
-
-
+            mines_array.c_mines(i, j, arr_mines)
 
 print("Welcome to Minesweeper!")
 print()
 
-print("0 1 2 3 4 5 6 7 8 9")
-
-for j in range(len(arr_game)):
-    for i in range(len(arr_game[j])):
-        print(arr_game[i][j], end=' ')
-    print(" " + str(j))
-
 print(input("Press Enter to Continue"))
 
 # placeholder to have a loop
-game_placeholder = 100
-while game_placeholder > 20:
+mark_count = 0
+block_count = 0
+
+while True:
+    mines_array.print_mines(arr_game)
+
     print("Entering C clears a spot and M marks it.")
     ask = input("Enter a command: ").upper()
 
@@ -52,17 +51,9 @@ while game_placeholder > 20:
 
     if ask == "C":
         mines_array.uncover(arr_mines, arr_game, int(x), int(y))
+
     elif ask == "M":
         mines_array.mark(arr_game, int(x), int(y))
-
-    print("0 1 2 3 4 5 6 7 8 9")
-    print()
-
-    for j in range(len(arr_game)):
-        for i in range(len(arr_game[j])):
-            print(arr_game[i][j], end=' ')
-        print(" " + str(j))
-#    print(input("Press Enter to Continue"))
-    game_placeholder -= 1
+        mark_count += 1
 
 print("You won!")
--- mines_array.py
+++ mines_array.py
@@ -1,45 +1,60 @@
 import sys
 
 
+def c_mines(x, y, mines):
+    count = 0
+    c = [x - 1, x, x + 1]
+    d = [y - 1, y, y + 1]
+
+    for a in c:
+        for b in d:
+            if a in range(10) and b in range(10):
+                if mines[a][b] == 9:
+                    count += 1
+
+    mines[x][y] = count
+
+
 def mark(game_map, x, y):
     game_map[x][y] = "M"
     return game_map
 
 
 def uncover(map_of_mines, game_map, x, y):
-    # for i in range(0, len(map_of_mines)):
-    #     map_of_mines[i] = int(map_of_mines[i])
 
     if map_of_mines[x][y] == 9:
-        print("KABLAM\nYou lost")
+        print("KABLAM!\nYou lost")
         for j in range(len(map_of_mines)):
             for i in range(len(map_of_mines[j])):
                 print(map_of_mines[i][j], end=' ')
                 print()
             sys.exit()
+
     elif map_of_mines[x][y] == 0:
         game_map[x][y] = str(0)
 
-        if x != 0 and game_map[x - 1][y] == "█":
-            uncover(map_of_mines, game_map, x - 1, y)
-        if x != 0 and y != 0 and game_map[x - 1][y - 1] == "█":
-            uncover(map_of_mines, game_map, x - 1, y - 1)
-        if x != 0 and y != 9 and game_map[x - 1][y + 1] == "█":
-            uncover(map_of_mines, game_map, x - 1, y + 1)
-        if y != 9 and game_map[x][y + 1] == "█":
-            uncover(map_of_mines, game_map, x, y + 1)
-        if x != 9 and y != 0 and game_map[x + 1][y - 1] == "█":
-            uncover(map_of_mines, game_map, x + 1, y - 1)
-        if x != 9 and game_map[x + 1][y] == "█":
-            uncover(map_of_mines, game_map, x + 1, y)
-        if x != 9 and y != 9 and game_map[x + 1][y + 1] == "█":
-            uncover(map_of_mines, game_map, x + 1, y + 1)
-        if y != 0 and game_map[x][y - 1] == "█":
-            uncover(map_of_mines, game_map, x, y - 1)
+        c = [x - 1, x, x + 1]
+        d = [y - 1, y, y + 1]
+
+        for a in c:
+            for b in d:
+                if a in range(len(map_of_mines)) and b in range(len(map_of_mines)):
+                    if game_map[a][b] == "█":
+                        uncover(map_of_mines, game_map, a, b)
 
     else:
         game_map[x][y] = str(map_of_mines[x][y])
     return game_map
+
+
+def print_mines(arr_game):
+    print("0 1 2 3 4 5 6 7 8 9")
+    print()
+
+    for j in range(len(arr_game)):
+        for i in range(len(arr_game[j])):
+            print(arr_game[i][j], end=' ')
+        print(" " + str(j))
 
 
 class Square:
